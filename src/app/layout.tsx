01d52f1d94a2a1a957bbfa729d0bd5edc2239ea6
NachoRodVil <ignaciorodvil@gmail.com>
2024-10-09 15:09:28 -0300

    added metadata, removed some global styles and added link to header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import Header from "@/components/header";
 import "./globals.css";
 import { QueryProvider } from "./queryProvider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Metafar Challenge',
+  description: 'Challenge para Metafar hecho por Ignacio Rodriguez Villasuso.',
+};
+
 
 export default function RootLayout({
   children,
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header: React.FC = () => {
     return (
         <header className="flex p-4">
+            <Link href={"/"}>
             <img src="/matafarLogo.svg" alt="Metafar Logo" className="h-12" />
+            </Link>
         </header>
     );
 };
